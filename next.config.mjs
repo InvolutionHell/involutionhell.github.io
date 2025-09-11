387dd56af80cf6ba26fdce7d6418621df4e6c4ef
Loong Loong <longsizhuo@gmail.com>
2025-09-11 23:31:03 +0800

    fix: fixed config error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 // next.config.mjs
 import { withContentlayer } from 'next-contentlayer'
 
-export default withContentlayer()({
+export default withContentlayer({
   reactStrictMode: true,
   output: 'export',         // 关键：静态导出到 /out
   images: { unoptimized: true }, // 避免使用 Next Image 优化服务
