23aa72508bdacbdcd11e99f9655b3e5b676b91d1
Loong Loong <longsizhuo@gmail.com>
2025-09-17 02:17:00 +0800

    fix：删除AI生成的Jobs页

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 import { Features } from "./components/Features";
-import { Jobs } from "./components/Jobs";
 import { Community } from "./components/Community";
 import { Footer } from "./components/Footer";
 
@@ -11,7 +10,6 @@ export default function DocsIndex() {
       <Header />
       <Hero />
       <Features />
-      <Jobs />
       <Community />
       <Footer />
     </>
